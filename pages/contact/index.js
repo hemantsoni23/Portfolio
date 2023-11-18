4132575c95d3661bafb3bffbcb6d4a5cb08da0fc
Hemant soni <sonih5190@gmail.com>
2023-11-18 22:45:16 +0530

    updating contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,43 +1,83 @@
 import { BsArrowRight } from 'react-icons/bs';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
-import BottomLeftImg from "../../components/BottomLeftImg";
+import BottomLeftImg from '../../components/BottomLeftImg';
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
 
   const [emailSubmitted, setEmailSubmitted] = useState(false);
 
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const data = {
-      email:e.target.email.value,
-      subject:e.target.subject.value,
-      message:e.target.message.value,
-    }
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
     const JSONdata = JSON.stringify(data);
     const endpoint = '/api';
 
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSONdata,
     };
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-    console.log(resData);
-    if (resData.status === "success") {
-      console.log("Message Sent.");
-      e.target.reset();
-      setEmailSubmitted(true);
-    } else if (resData.status === "fail") {
-      console.log("Message failed to send.");
+    try {
+      const response = await fetch(endpoint, options);
+      const resData = await response.json();
+
+      if (resData.success) {
+        console.log('Message Sent.');
+        e.target.reset();
+        setEmailSubmitted(true);
+
+        // Display success toast
+        toast.success('Email sent successfully!', {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+        });
+      } else {
+        console.log('Message failed to send.');
+
+        // Display error toast
+        toast.error('Failed to send email. Please try again later.', {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+
+      // Display error toast
+      toast.error('Failed to send email. Please try again later.', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
     }
-    }
+  };
   return (
     <div className='h-full bg-primary/30'>
+      <ToastContainer />
       <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
       <div className='w-full h-full bottom-0  bg-moon bg-cover bg-center bg-no-repeat absolute mix-blend-color-dodge '>
         <BottomLeftImg/>
@@ -53,14 +93,9 @@ const Contact = () => {
             <textarea id='message' placeholder='message' className=' textarea'></textarea>
             <button type='submit' className='btn rounded-full border border-white max-w-[170px] px-8 transition-all duration-300 flex items-center
           justify-center overflow-hidden hover:before-accent group'>
-              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500 font-normal'>Let&apos;s talk</span>
+              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500 font-normal'>Send Mail</span>
               <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
             </button>
-            {
-              emailSubmitted && (
-                <div className='text-accent font-bold text-center'>Email Sent</div>
-              )
-            }
           </motion.form>
         </div>
       </div>
