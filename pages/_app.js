6e5a03aac7184bdd0e65aec4c2f0fba73ea08220
Hemant soni <sonih5190@gmail.com>
2023-11-21 18:33:17 +0530

    removing transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
-import Transition from '../components/Transition';
+// import Transition from '../components/Transition';
 import { useRouter } from 'next/router';
 import { AnimatePresence, motion } from 'framer-motion';
 
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <AnimatePresence mode='wait'>
           <motion.div key={router.route} className='h-full'>
-            <Transition />
+            {/* <Transition /> */}
             <Component {...pageProps} />
           </motion.div>
         </AnimatePresence>
