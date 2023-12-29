a833346361d8ea4e9541b6008279b96d218dbe18
Hemant soni <sonih5190@gmail.com>
2023-12-29 11:55:15 +0000

    removing motin from resume

--- components/ResumeBtn.js
+++ components/ResumeBtn.js
@@ -1,6 +1,4 @@
 import { RiGraduationCapLine } from 'react-icons/ri';
-import { motion } from 'framer-motion';
-import { fadeIn } from '../variants';
 
 const ResumeBtn = () => {
   const handleDownloadResume = () => {
@@ -11,13 +9,7 @@ const ResumeBtn = () => {
   };
 
   return (
-    <motion.div
-      variants={fadeIn('up', 0.4)}
-      initial='hidden'
-      animate='show'
-      exit='hidden'
-      className='flex-1 flex flex-col gap-6 w-full mx-auto'
-    >
+    <div className='flex-1 flex flex-col gap-6 w-full mx-auto'>
       <button
         className='btn rounded-full border border-white max-w-[170px] px-8 transition-all duration-300 flex items-center
         justify-center overflow-hidden hover:before-accent group'
@@ -28,7 +20,7 @@ const ResumeBtn = () => {
         </span>
         <RiGraduationCapLine className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
       </button>
-    </motion.div>
+    </div>
   );
 };
 
